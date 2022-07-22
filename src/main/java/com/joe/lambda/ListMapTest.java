639a0110d7c5c1d9a6f1b9d810322d84ca0b483d
xqh <1187773467@qq.com>
2022-07-22 09:43:01 +0800

    lambda合并重复数据

--- src/main/java/com/joe/lambda/ListMapTest.java
+++ src/main/java/com/joe/lambda/ListMapTest.java
@@ -14,24 +14,24 @@ public class ListMapTest {
 
         Map<String,Object> map1 = new HashMap<>();
         map1.put("id",1);
-        map1.put("target_value",1);
+        map1.put("target_value",100);
 
         list.add(map1);
 
         Map<String,Object> map2 = new HashMap<>();
         map2.put("id",2);
-        map2.put("target_value",1);
+        map2.put("target_value",200);
 
         list.add(map2);
 
         Map<String,Object> map3 = new HashMap<>();
         map3.put("id",1);
-        map3.put("target_value",1);
+        map3.put("target_value",300);
 
         list.add(map3);
 
         //根据id重复合并重复数据
-        List<Map<String, Object>> mapList = list.stream().collect(Collectors.toMap(a -> a, v -> v, (o1, o2) -> {
+        List<Map<String, Object>> mapList = list.stream().collect(Collectors.toMap(a -> a.get("id"), v -> v, (o1, o2) -> {
             if (o1.get("id").equals(o2.get("id"))) {
                 Integer v1 = 0;
                 Integer v2 = 0;
